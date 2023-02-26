47f68840d4b572ec1c5b770b4704692cea87deb7
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2023-02-27 04:16:37 +0630

    Add project files.

---
+++ BlazorWasm.ExpenseTracker/Models/TransactionModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorWasm.ExpenseTracker.Models
+{
+    public class TransactionModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public DateTime ModifiedDateTime { get; set; }
+        public string TransactionType { get; set; }
+    }
+}
---
+++ BlazorWasm.ExpenseTracker/Pages/Index.razor.cs
@@ -0,0 +1,77 @@
+﻿using BlazorWasm.ExpenseTracker.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using System.Transactions;
+
+namespace BlazorWasm.ExpenseTracker.Pages
+{
+    public partial class Index
+    {
+        private string Amount = string.Empty;
+
+        private TransactionModel transaction = new TransactionModel();
+        private List<TransactionModel> transactions = new List<TransactionModel>();
+
+        protected override void OnInitialized()
+        {
+            transaction.TransactionType = "Expense";
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await List();
+                await _jsRuntime.InvokeVoidAsync("handleKeyPress", "amount");
+            }
+        }
+
+        async Task AddTransaction()
+        {
+            //var data = await _jsRuntime.InvokeAsync<string>("getTextboxValue", "amount");
+            //transaction.Amount = Convert.ToDecimal(data.ToString());
+            transaction.Amount = Convert.ToDecimal(Amount);
+            transaction.CreatedDateTime = DateTime.Now;
+            transaction.ModifiedDateTime = DateTime.Now;
+            await List();
+            transaction.Id = transactions.Count == 0 ? 1 : transactions.Max(x => x.Id) + 1;
+            transactions.Add(transaction);
+            await _jsRuntime.InvokeAsync<string>("localStorage.setItem", "Transactions", JsonConvert.SerializeObject(transactions));
+            Amount = string.Empty;
+            transaction = new TransactionModel();
+        }
+
+        async Task RemoveTransaction(int id)
+        {
+            await List();
+            var item = transactions.FirstOrDefault(x => x.Id == id);
+            if (item == null) return;
+            transactions.Remove(item);
+            await _jsRuntime.InvokeAsync<string>("localStorage.setItem", "Transactions", JsonConvert.SerializeObject(transactions));
+            await List();
+        }
+
+        async Task List()
+        {
+            var jsonStr = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "Transactions");
+            if (jsonStr != null)
+            {
+                transactions = JsonConvert.DeserializeObject<List<TransactionModel>>(jsonStr);
+                StateHasChanged();
+            }
+        }
+
+        void TransactionTypeChange(ChangeEventArgs args)
+        {
+            transaction.TransactionType = args.Value.ToString();
+        }
+
+        private string Total => (transactions.Where(x => x.TransactionType == "Income").Sum(x => x.Amount) +
+            (transactions.Where(x => x.TransactionType == "Expense").Sum(x => x.Amount) * -1)).ToString("n2");
+
+        private string Income => transactions.Where(x => x.TransactionType == "Income").Sum(x => x.Amount).ToString("n2");
+
+        private string Expense => transactions.Where(x => x.TransactionType == "Expense").Sum(x => x.Amount).ToString("n2");
+    }
+}
